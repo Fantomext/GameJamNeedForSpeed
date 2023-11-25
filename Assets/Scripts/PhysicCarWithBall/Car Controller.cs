9902800f286205b55c94a3b8f5930eba9f33e6b3
Fantomext <Fantomext80@gmail.com>
2023-11-25 20:18:34 +0300

    NewCarUpdated

--- Assets/Scripts/PhysicCarWithBall/Car Controller.cs
+++ Assets/Scripts/PhysicCarWithBall/Car Controller.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody _rigibody;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float _maxSpeed  = 50f;
+    [SerializeField] private float _forwardAccel = 8f;
+    [SerializeField] private float _reverseAccel = 4f;
+    [SerializeField] private float _turnStrength = 180f;
+
+    private float _horizontal;
+    private float _vertical;
+    private void Start()
     {
-        
+        _rigibody.transform.parent = null;
+    }
+    private void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+        transform.position = _rigibody.transform.position;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f,_horizontal * _turnStrength * Time.deltaTime,0f));
+
+    }
+    private void FixedUpdate()
+    {
+        _rigibody.AddForce(transform.forward * _vertical * _forwardAccel * 100);
     }
 }
