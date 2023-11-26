99ab5140e48d9e46c98cbaaf65f4936b444582d2
Leonid Vorobev <extrasparrow@gmail.com>
2023-11-26 20:04:38 +0300

    Terrain Painter

--- Assets/Scripts/TerrainChanger.cs
+++ Assets/Scripts/TerrainChanger.cs
@@ -1,4 +1,5 @@
 using EasyRoads3Dv3;
+using sc.terrain.proceduralpainter;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,12 +7,14 @@ using UnityEngine;
 public class TerrainChanger : MonoBehaviour
 {
     [SerializeField] private Terrain terrain;
-    TerrainData _terrainData;
+    [SerializeField] TerrainData _terrainData;
     float[,,] alphamaps;
     public int value = 0;
     public int valuetwo = 0;
     private void Start()
     {
+        //рср реппщим оеимреп
+        TerrainPainter painter = new TerrainPainter();
         _terrainData = terrain.terrainData;
         float[,,] alphamaps = _terrainData.GetAlphamaps(0, 0, _terrainData.alphamapWidth, _terrainData.alphamapHeight);
     }
@@ -24,9 +27,9 @@ public class TerrainChanger : MonoBehaviour
         {
             for (int y = 0; y < _terrainData.alphamapWidth; y++)
             {
-                alphamaps[i, y, 0] = value;
-                alphamaps[i, y, 1] = valuetwo;
-
+                alphamaps[i, y, 0] = 0;
+                alphamaps[i, y, 1] = 0;
+                alphamaps[i, y, 2] = 0;
             }
         }
 
